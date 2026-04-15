df5fea0deaf56524b1062b5423d0a5320a932e3f
Marek Felšöci <marek@felsoci.sk>
2026-04-15 23:59:02 +0200

    Préparer l'implémentation du test unitaire de « horsForfait »

---
+++ tests/unit_tests_location.py
@@ -0,0 +1,9 @@
+import unittest
+from velivelo.location import *
+
+class TestHorsForfait(unittest.TestCase):
+  def test_exemple(self):
+    self.assertEqual(True, False)
+
+if __name__ == '__main__':
+  unittest.main()
