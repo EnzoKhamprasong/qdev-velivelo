8964e31df66b41efd6e1a13d9e28ac65c1359afc
Enzo Khamprasong <22503774@me.uvsq.fr>
2026-04-16 11:57:56 +0200

    Ajout de 'def' dans le fichier des test de location pour mieux voir les erreurs

--- tests/unit_tests_location.py
+++ tests/unit_tests_location.py
@@ -2,10 +2,13 @@ import unittest
 from velivelo.location import *
 
 class TestHorsForfait(unittest.TestCase):
-  def test_exemple(self):
+  def test_SansDepassement(self):
     self.assertEqual(horsForfait(60,90), Verdict.SANS_DEPASSEMENT)
+  def test_AvecDepassement(self):
     self.assertEqual(horsForfait(60,30), Verdict.AVEC_DEPASSEMENT)
+  def test_Amende(self):
     self.assertEqual(horsForfait(241,380), Verdict.AMENDE)
+  def test_Error(self):
     self.assertRaises(ValueError,horsForfait,0,30)
 
 if __name__ == '__main__':
