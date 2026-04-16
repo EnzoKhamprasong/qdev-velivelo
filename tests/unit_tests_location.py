f28a8941be4e2af102c2cd83c487297795efce05
Enzo Khamprasong <22503774@me.uvsq.fr>
2026-04-16 11:29:34 +0200

    Ajout du test du cas error

--- tests/unit_tests_location.py
+++ tests/unit_tests_location.py
@@ -6,6 +6,7 @@ class TestHorsForfait(unittest.TestCase):
     self.assertEqual(horsForfait(60,90), Verdict.SANS_DEPASSEMENT)
     self.assertEqual(horsForfait(60,30), Verdict.AVEC_DEPASSEMENT)
     self.assertEqual(horsForfait(241,380), Verdict.AMENDE)
+    self.assertRaises(ValueError,horsForfait,0,30)
 
 if __name__ == '__main__':
   unittest.main()
