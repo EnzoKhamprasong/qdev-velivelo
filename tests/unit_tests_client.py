a42e9e22831bda33f8db55101920fb9e25a03084
Enzo Khamprasong <22503774@me.uvsq.fr>
2026-04-16 11:56:09 +0200

    Ajout de 'def' dans le fichier test pour mieux voir les erreurs

--- tests/unit_tests_client.py
+++ tests/unit_tests_client.py
@@ -2,12 +2,14 @@ import unittest
 from velivelo.client import *
 
 class TestPseudoClientConforme(unittest.TestCase):
-  def test_exemple(self):
+  def test_pseudoConforme(self):
     self.assertEqual(pseudoClientConforme("Rob96"), True)
+  def test_pseudoNonConforme(self):
     self.assertEqual(pseudoClientConforme("Ro"), False)
     self.assertEqual(pseudoClientConforme("Rob96666666"), False)
     self.assertEqual(pseudoClientConforme("rob96"), False)
     self.assertEqual(pseudoClientConforme("Rob96%"), False)
+  def test_pseudoError(self):
     self.assertRaises(ValueError,pseudoClientConforme,"")
 
 if __name__ == '__main__':
