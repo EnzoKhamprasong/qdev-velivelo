2a1073693b56f489bbf063e13d978cdbd4c22a8e
Enzo Khamprasong <22503774@me.uvsq.fr>
2026-04-16 11:41:10 +0200

    Implémentation du cas Faux où le pseudo à moins de 3 ou plus de 10 caracteres

--- velivelo/client.py
+++ velivelo/client.py
@@ -1,3 +1,5 @@
 def pseudoClientConforme(pseudo : str) -> bool:
   if pseudo == "":
     raise ValueError("Le pseudo ne doit pas être vide")
+  elif len(pseudo)<3 or len(pseudo)>10:
+    return False
