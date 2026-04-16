0f673522c5b1e29c9608b941c74b2e617025df64
Enzo Khamprasong <22503774@me.uvsq.fr>
2026-04-16 11:14:29 +0200

    Ajout des tests valides

--- tests/unit_tests_location.py
+++ tests/unit_tests_location.py
@@ -3,7 +3,9 @@ from velivelo.location import *
 
 class TestHorsForfait(unittest.TestCase):
   def test_exemple(self):
-    self.assertEqual(True, False)
+    self.assertEqual(horsForfait(60,90), "SansDépassement")
+    self.assertEqual(horsForfait(60,30), "AvecDépassement")
+    self.assertEqual(horsForfait(241,380), "Amende")
 
 if __name__ == '__main__':
   unittest.main()
