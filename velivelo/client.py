0fb5b437f027284acbcc26023d4eaff01556276d
Enzo Khamprasong <22503774@me.uvsq.fr>
2026-04-16 11:43:35 +0200

    Implémentation du cas Faux où le pseudo ne commence pas par une maj

--- velivelo/client.py
+++ velivelo/client.py
@@ -3,3 +3,5 @@ def pseudoClientConforme(pseudo : str) -> bool:
     raise ValueError("Le pseudo ne doit pas être vide")
   elif len(pseudo)<3 or len(pseudo)>10:
     return False
+  elif pseudo[0].isupper() == False: # s'il ne commence pas par une MAJ
+    return False
