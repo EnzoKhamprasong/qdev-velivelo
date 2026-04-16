584b71160fa53804f8b6adc2d0af2d43b7d4d17d
Enzo Khamprasong <22503774@me.uvsq.fr>
2026-04-16 11:16:51 +0200

    Correction des tests valides

--- tests/unit_tests_location.py
+++ tests/unit_tests_location.py
@@ -3,9 +3,9 @@ from velivelo.location import *
 
 class TestHorsForfait(unittest.TestCase):
   def test_exemple(self):
-    self.assertEqual(horsForfait(60,90), "SansDépassement")
-    self.assertEqual(horsForfait(60,30), "AvecDépassement")
-    self.assertEqual(horsForfait(241,380), "Amende")
+    self.assertEqual(horsForfait(60,90), Verdict.SANS_DEPASSEMENT)
+    self.assertEqual(horsForfait(60,30), Verdict.AVEC_DEPASSEMENT)
+    self.assertEqual(horsForfait(241,380), Verdict.AMENDE)
 
 if __name__ == '__main__':
   unittest.main()
