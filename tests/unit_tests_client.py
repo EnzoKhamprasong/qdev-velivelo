b71ed17d27c0eb88b0d521a7d00b0e70194a3bd7
Enzo Khamprasong <22503774@me.uvsq.fr>
2026-04-16 11:33:06 +0200

    Ajout des tests unitaires pour les cas valides

--- tests/unit_tests_client.py
+++ tests/unit_tests_client.py
@@ -3,7 +3,11 @@ from velivelo.client import *
 
 class TestPseudoClientConforme(unittest.TestCase):
   def test_exemple(self):
-    self.assertEqual(True, False)
+    self.assertEqual(pseudoClientConforme("Rob96"), True)
+    self.assertEqual(pseudoClientConforme("Ro"), False)
+    self.assertEqual(pseudoClientConforme("Rob96666666"), False)
+    self.assertEqual(pseudoClientConforme("rob96"), False)
+    self.assertEqual(pseudoClientConforme("Rob96%"), False)
 
 if __name__ == '__main__':
   unittest.main()
